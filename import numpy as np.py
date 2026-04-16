012b9fa8899dfbc6c914f2502be44cd6d14a0662
Your Name <you@example.com>
2026-04-16 21:21:46 +0000

    Initial project upload

---
+++ import numpy as np.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+import seaborn as sns
+
+print("Everything is working")
+print(np.__version__)
+print(pd.__version__)
+print(sklearn.__version__)
